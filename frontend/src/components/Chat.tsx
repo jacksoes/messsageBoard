aff33de12b3a01a852b3b07b7f7fa388c0ae2f2e
jack soesman <jsoesman1@yahoo.com>
2025-04-22 23:30:15 -0400

    finish chat styling

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -3,12 +3,22 @@ import "./Chat.css"
 const Chat = () =>{
     return(
     <div className="container">
+
+        <div className="chat-display">
+            <span className="text-content">Jack: i am typing in the chat</span>
+            <br/>
+            <br/>
+            <span className="text-content">User2: i am also typign in chat</span>
+            
+         
+            
+        </div>
         
         <div className="chat-input-container">
 
             <input className="chat-input"></input>
 
-            <button className="chat-button">chat</button>
+            <button className="chat-button">Chat</button>
         </div>
         
         
